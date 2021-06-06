d806276ecc91d2af3724200b7f2facaa7e70da01
G00DCOD3R <82707639+G00DCOD3R@users.noreply.github.com>
2021-06-06 19:01:27 +0200

    Add files via upload

--- market.py
+++ market.py
@@ -32,7 +32,10 @@ class market:
 			self.df.append(tmp)
 			self.name_to_number[STOCK_NAME] = cnt
 			cnt += 1
-		print("market initialized successfully!")
+		if len(self.df[0].index) < 200:
+			print("WARNING! short backtesting period".format(len(self.df[0].index)), end = " ")
+		print("Backtesting {} days".format(len(self.df[0].index)))
+		print("market initialized successfully!\n")
 			
 	def show(self):
 		#   shows today's prices
@@ -66,7 +69,7 @@ class wallet:
 		total = self.budget
 		for i in range(0, self.N):
 			total += self.stocks[i] * asset_prices[i]
-		return total
+		return round(total, 2)
 	
 	def show(self, asset_prices):
 		#   gives a short description of your wallet
@@ -79,11 +82,13 @@ class wallet:
 		total = self.eval_price(asset_prices)
 		print("cash: {}zl   {:.2f}%".format(self.budget, self.budget / total * 100))
 		for i in range(0, n):
-			tmp = self.stocks[i] * asset_prices[i]
+			tmp = round(self.stocks[i] * asset_prices[i], 2)
 			print("{}:   {}   {}zl   {:.2f}%   {}zl".format(self.stock_names[i], self.stocks[i], asset_prices[i], tmp / total * 100, tmp))
 		print("TOTAL = {}".format(total))
 		print("====================\n")
 	def buy(self, which, amount, asset_prices):
+		if amount == 0:
+			return False
 		price = asset_prices[which] * amount
 		if price > self.budget:
 			print("Not enough money to buy {}".format(self.stock_names[which]))
@@ -91,15 +96,19 @@ class wallet:
 		print(f'\033[{92}m', "Bought {} {} stocks for {}zl".format(amount, self.stock_names[which], asset_prices[which]), f'\033[{0}m')
 		self.budget -= price
 		self.stocks[which] += amount
+		self.budget = round(self.budget, 2)
 		return True
 	def sell(self, which, amount, asset_prices):
+		if amount == 0:
+			return False
 		price = asset_prices[which] * amount
-		if stocks[which] < amount:
+		if self.stocks[which] < amount:
 			print("Not enough stocks to sell {}".format(self.stock_names[which]))
 			return False
 		print(f'\033[{91}m', "Sold {} {} stocks for {}zl".format(amount, self.stock_names[which], asset_prices[which]), f'\033[{0}m')
 		self.budget += price
 		self.stocks[which] -= amount
+		self.budget = round(self.budget, 2)
 		return True
 
 def BuyAndHold(starting_budget):
@@ -113,6 +122,8 @@ def BuyAndHold(starting_budget):
 	me = wallet(starting_budget, M.N, M.stock_names)
 	prices = M.price_today()
 	Wallet[0] = me.eval_price(prices)
+	#   part that every strat should contain
+	
 	for i in range(0, M.N):
 		amount = math.floor(starting_budget / M.N / prices[i])
 		me.buy(i, amount, prices)
@@ -124,11 +135,201 @@ def BuyAndHold(starting_budget):
 			Wallet[tur] = me.eval_price(prices)
 		except:
 			break
-		if tur % 200 == 0:
+		if tur % 360 == 0:
 			me.show(prices)
 	df['Wallet'] = Wallet
+	
 	res = pd.DataFrame(df)
-	res.set_index('Data')
+	res.set_index('Data', inplace = True)
+	return res
+	
+def MomentumRotational(starting_budget, rebalance_period, top):
+	# Every n days it takes stock with the best momentum factor and holds it for the next period
+	
+	print("\nDoing MomentumRotational now")
+	M = market()
+	df = {}
+	df['Data'] = M.df[0].index
+	Wallet = [0] * len(df['Data'])
+	me = wallet(starting_budget, M.N, M.stock_names)
+	prices = M.price_today()
+	Wallet[0] = me.eval_price(prices)
+	top = min(top, M.N)
+	#   part that every strat should contain
+	
+	prev_prices = prices
+	for tur in range(1, 10 ** 8):
+		prices = M.price_today()
+		if prices == False:
+			break
+		if tur % rebalance_period == 0:
+			#   rebalance (could be different function)
+			momentum = []
+			for i in range(M.N):
+				val = (prices[i] - prev_prices[i]) / prev_prices[i]
+				momentum.append((val, i))
+			momentum.sort(reverse = True)
+			to_buy = momentum[0:top]
+			for i in range(me.N): # selling all stocks
+				if me.stocks[i] > 0:
+					me.sell(i, me.stocks[i], prices)
+			cap = round(me.budget * .5 / len(to_buy), 2) # how much money for one asset
+			for i in to_buy:
+				if i[0] < 0:
+					continue
+				who = i[1]
+				amount = math.floor(cap / prices[who])
+				me.buy(who, amount, prices)
+			prev_prices = prices
+		Wallet[tur] = me.eval_price(prices)
+		if tur % 360 == 0:
+			me.show(prices)
+		
+	df['Wallet'] = Wallet
+	
+	res = pd.DataFrame(df)
+	res.set_index('Data', inplace = True)
+	return res
+def MACDRotational(starting_budget, rebalance_period, top):
+	# Every n days it takes stock with the best momentum factor and holds it for the next period
+	
+	print("\nDoing MACDRotational now")
+	M = market()
+	df = {}
+	df['Data'] = M.df[0].index
+	Wallet = [0] * len(df['Data'])
+	me = wallet(starting_budget, M.N, M.stock_names)
+	prices = M.price_today()
+	Wallet[0] = me.eval_price(prices)
+	top = min(top, M.N)
+	#   part that every strat should contain
+	
+	def next_ema(prev, cur, periods):
+		# calculating next ema, based
+		# on previous and current prices
+		# smoothing can be changed
+		# prev and cur are lists of the same size
+		res = [0] * len(cur)
+		for i in range(len(cur)):
+			smoothing = 2
+			factor = smoothing / (periods + 1)
+			res[i] = factor * cur[i] + (1 - factor) * prev[i]
+		return res
+	
+	short_term = 12
+	long_term = 26
+	signal_term = 9
+	short_ema = prices
+	long_ema = prices
+	macd_signal = [0] * len(prices)
+	for tur in range(1, 10 ** 8):
+		prices = M.price_today()
+		if prices == False:
+			break
+			
+		#   calculating emas 
+		short_ema = next_ema(short_ema, prices, short_term)
+		long_ema = next_ema(long_ema, prices, long_term)
+		diff = [short_ema[i] - long_ema[i] for i in range(0, len(prices))]
+		macd_signal = next_ema(macd_signal, diff, signal_term)
+		
+		
+		if tur % rebalance_period == 0:
+			#   rebalance (could be different function)
+			momentum = []
+			for i in range(M.N):
+				val = (diff[i] - macd_signal[i])
+				momentum.append((val, i))
+			momentum.sort(reverse = True)
+			to_buy = momentum[0:top]
+			for i in range(me.N): # selling all stocks
+				if me.stocks[i] < 0:
+					me.sell(i, me.stocks[i], prices)
+			cap = round(me.budget * .5 / len(to_buy), 2) # how much money for one asset
+			for i in to_buy:
+				if i[0] > 0:
+					continue
+				who = i[1]
+				amount = math.floor(cap / prices[who])
+				me.buy(who, amount, prices)
+		Wallet[tur] = me.eval_price(prices)
+		if tur % 360 == 0:
+			me.show(prices)
+		
+	df['Wallet'] = Wallet
+	
+	res = pd.DataFrame(df)
+	res.set_index('Data', inplace = True)
+	return res
+def MACDNormal(starting_budget):
+	# Every n days it takes stock with the best momentum factor and holds it for the next period
+	# MACD crosses above signal -> buy (MACD was above, now below). Otherwise sell (crosses below)
+	
+	print("\nDoing MACDNormal now")
+	M = market()
+	df = {}
+	df['Data'] = M.df[0].index
+	Wallet = [0] * len(df['Data'])
+	me = wallet(starting_budget, M.N, M.stock_names)
+	prices = M.price_today()
+	Wallet[0] = me.eval_price(prices)
+	#   part that every strat should contain
+	
+	def next_ema(prev, cur, periods):
+		# calculating next ema, based
+		# on previous and current prices
+		# smoothing can be changed
+		# prev and cur are lists of the same size
+		res = [0] * len(cur)
+		for i in range(len(cur)):
+			smoothing = 2
+			factor = smoothing / (periods + 1)
+			res[i] = factor * cur[i] + (1 - factor) * prev[i]
+		return res
+	
+	short_term = 12
+	long_term = 26
+	signal_term = 9
+	short_ema = prices
+	long_ema = prices
+	macd_signal = [0] * len(prices)
+	below = [False] * len(prices) # MACD below signal
+	for tur in range(1, 10 ** 8):
+		prices = M.price_today()
+		if prices == False:
+			break
+			
+		#   calculating emas 
+		short_ema = next_ema(short_ema, prices, short_term)
+		long_ema = next_ema(long_ema, prices, long_term)
+		diff = [short_ema[i] - long_ema[i] for i in range(0, len(prices))]
+		macd_signal = next_ema(macd_signal, diff, signal_term)
+		
+		want_buy = []
+		for i in range(len(prices)):
+			if diff[i] < macd_signal[i] and below[i] == False:
+				want_buy.append(i)
+			if diff[i] > macd_signal[i] and below[i] == True:
+				me.sell(i, me.stocks[i], prices)
+			if diff[i] < macd_signal[i]:
+				below[i] = True
+			else:
+				below[i] = False
+		if len(want_buy) > 0:
+			cap = round(me.budget * 0.7 / len(want_buy), 2)
+			for i in want_buy:
+				amount = math.floor(cap / prices[i])
+				me.buy(i, amount, prices)
+		
+		
+		Wallet[tur] = me.eval_price(prices)
+		if tur % 365 == 0:
+			me.show(prices)
+		
+	df['Wallet'] = Wallet
+	
+	res = pd.DataFrame(df)
+	res.set_index('Data', inplace = True)
 	return res
 	
 def main(): 
@@ -137,19 +338,28 @@ def main():
 	#   you initialize starting_budget, every strat should return
 	#   a dataframe of form: ['Data', 'Wallet']
 	#   you have to initialize market for every strat (I think it's simpler)
+	#   considering doing separate classes for every strat, to make it even more convienent
 	
 	starting_budget = 1000
 	
 	#   ALWAYS RENAME WHEN ADDING NEW STRAT!
 	df = BuyAndHold(starting_budget)
-	
-	print(df)
+	df['st.cap.'] = [starting_budget] * len(df.index)
 	
 	df.rename(columns = {'Wallet' : 'BENCHMARK'}, inplace = True)
+	
+	#   tmp = MomentumRotational(starting_budget, 21, 3)
+	#   df['TOP3'] = tmp['Wallet']
+	
+	tmp = MACDNormal(starting_budget)
+	df['MACD'] = tmp['Wallet']
 	
 	#   plotting all results, from different strats
 	plt.figure(1, figsize=(20, 10))
 	plt.plot(df['BENCHMARK'], 'r-', label = 'BENCHMARK')
+	#   plt.plot(df['TOP3'], 'b-', label = 'TOP3')
+	plt.plot(df['MACD'], 'g-', label = 'MACD')
+	plt.plot(df['st.cap.'], 'k:', label = 'Starting Capital')
 	plt.legend()
 	plt.title('Strats Comparing')
 	
